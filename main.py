d6d495871fe49eaf75020fe648fc4d662935c0d9
Sabareesh Vishwanathan <61770466+svishwanathan01@users.noreply.github.com>
2020-09-28 15:24:09 -0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,44 @@
+from geopy.geocoders import Nominatim
+import math, csv
+
+
+def haversine(lat1, lon1, lat2, lon2):
+    radius = 3959.87433 #miles
+
+    dlat = math.radians(lat2-lat1)
+    dlon = math.radians(lon2-lon1)
+    a = math.sin(dlat/2) * math.sin(dlat/2) + math.cos(math.radians(lat1)) \
+        * math.cos(math.radians(lat2)) * math.sin(dlon/2) * math.sin(dlon/2)
+    c = 2 * math.atan2(math.sqrt(a), math.sqrt(1-a))
+    d = radius * c
+
+    return round(d, 4)
+
+def distance(e):
+    return e['Distance']
+
+def appendSortCSV(tempList):
+    destList = []
+    geolocator = Nominatim(user_agent="Hello")
+    origin = geolocator.geocode("Stony Brook University")
+    for pos in range(len(tempList)):
+        # print tempList[pos]
+        dest = geolocator.geocode(tempList[pos])
+        destList.append({'Address': tempList[pos], 'Distance': float(
+            haversine(origin.latitude, origin.longitude, dest.latitude, dest.longitude))})
+    destList.sort(key=distance)
+    for pos in range(len(destList)):
+        print destList[pos]
+
+def main():
+    with open('redfin_2020-09-04-13-52-21 updated9.csv', mode='r') as csv_file:
+        csv_f = csv.DictReader(csv_file)
+        tempList = []
+        for row in csv_f:
+            tempList.append(row['ADDRESS'] + ", " + row['CITY'] + ", " + row['STATE OR PROVINCE'] + ", " + row['ZIP OR POSTAL CODE'])
+    return appendSortCSV(tempList)
+
+
+
+
+main()
